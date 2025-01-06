6673537a03c8f1fa38ecf5f895c96dfc7f567f79
pankaj verma <pankaj912978@gmail.com>
2025-01-06 23:07:01 +0530

    push

---
+++ Assignment01/7.js
@@ -0,0 +1,3 @@
+console.log("%d")
+console.log("\\n")
+console.log("\\")
---
+++ Assignment01/8.js
@@ -0,0 +1,9 @@
+// Program to take a date input in "DD/MM/YYYY" format and convert it
+
+const  inputDate = "06/01/2025"
+
+const [day,month,year] = inputDate.split("/");
+
+console.log(`day-${day}, month-${month}, year-${year}`)
+
+
